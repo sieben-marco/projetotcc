5b5c5bb9f70057c920169667d976ec08ad83a5d6
marco <57383943+sieben-marco@users.noreply.github.com>
2023-09-10 05:06:45 -0300

    Revert "corringo erro php login.php"
    This reverts commit 6e69c6b81a9810a0d6878ebdf2b4da3a557e343a.

--- login.php
+++ login.php
@@ -1,32 +1,3 @@
-<?php
-    // print_r($_REQUEST);
-
-    if (isset($_GET["submit"]) /*&& !empty($_GET["email"]) && !empty($_GET["senha"])*/) {
-        include_once("./script/config.php");
-        $email = $_GET['email'];
-        $senha = $_GET['senha'];
-
-        // print_r("<br>Email: " . $email);
-        // print_r("<br>");
-        // print_r("Senha: " . $senha);
-
-        $sql = "select * from teste_cliente where email = '$email' and senha = '$senha'";
-
-        $result = $conexao->query($sql);
-
-        // print_r($result);
-
-        $mostra_erro = mysqli_num_rows($result);
-
-        if (mysqli_num_rows($result) < 1) {
-            header("Location: ./login.php");
-        } else {
-            header("Location: ./sistema.php");
-        }
-    } /*else {
-            header("location: ./login.php");
-        }*/
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -41,15 +12,44 @@
 
         <header>
             <div id="logo" onclick="javascript:document.location.href='./index.html'">
-                <img class="logo" src="assets/logo azul.png" alt="logo do app aqui">
+                <img class="logo" src="assets/logo azul.png" alt="logo do app aqui"> 
             </div>
 
             <h3 class="nomeapp">Control-AR</h3>
         </header>
 
         <main>
+            <?php
+                // print_r($_REQUEST);
 
-            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+                if (isset($_GET["submit"]) /*&& !empty($_GET["email"]) && !empty($_GET["senha"])*/) {
+                    include_once("./script/config.php");
+                    $email = $_GET['email'];
+                    $senha = $_GET['senha'];
+
+                    // print_r("<br>Email: " . $email);
+                    // print_r("<br>");
+                    // print_r("Senha: " . $senha);
+
+                    $sql = "select * from teste_cliente where email = '$email' and senha = '$senha'";
+
+                    $result = $conexao->query($sql);
+
+                    // print_r($result);
+
+                    $mostra_erro = mysqli_num_rows($result);
+
+                    if(mysqli_num_rows($result) < 1) {
+                        header("Location: ./login.php");
+                    } else {
+                        header("Location: ./sistema.php");
+                    }
+                } /*else {
+                    header("location: ./login.php");
+                }*/
+            ?>
+
+            <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
                 <div class="form-group">
                     <label for="email" class="col-form-label">Insira seu email: </label>
                     <input type="email" class="form-control" name="email" id="email" placeholder="Digite seu email">
